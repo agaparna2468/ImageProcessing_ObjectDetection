652da1c494256af199235b4cf1c33d4d01949dcb
Aparna Agrawal <98680990+agaparna2468@users.noreply.github.com>
2023-07-09 23:56:35 +0530

    Update Porosity.py

--- ModelAssignment/Porosity.py
+++ ModelAssignment/Porosity.py
@@ -8,18 +8,17 @@ retval, img_thresh = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY)
 x, y = img_thresh.shape
 total_no = x*y
 pores = total_no - np.count_nonzero(img_thresh == 255)
-print(np.count_nonzero(img_thresh == 255))
-print(total_no)
-print(pores)
-print(pores/total_no)
+#print(np.count_nonzero(img_thresh == 255))
+#print(total_no)
+#print(pores)
+print(f'Porosity calculated using binary thresholding with intensities below 60 are given 0: {pores/total_no}')
 
 img_thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 7)
 
 x, y = img_thresh.shape
 total_no = x*y
 pores = total_no - np.count_nonzero(img_thresh == 255)
-print(np.count_nonzero(img_thresh == 255))
-print(total_no)
-print(pores)
-print(pores/total_no)
-
+#print(np.count_nonzero(img_thresh == 255))
+#print(total_no)
+#print(pores)
+print(f'Porosity calculated using binary thresholding with gaussian thresholding around a neighbourhood of 11 and constant as 7: {pores/total_no}')
